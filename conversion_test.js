30b88e4b0b8cb6827fa959dc744cc92e028b90cf
Dennis Martinez <dennis@dennmart.com>
2020-02-20 15:08:34 +0900

    Update mock; Tests pass now

--- conversion_test.js
+++ conversion_test.js
@@ -5,12 +5,16 @@ fixture("Cash Conversion App").page("http://localhost:3000/");
 
 const conversionMock = RequestMock()
   .onRequestTo("http://localhost:3001/exchange_rates/convert")
-  .respond({
-    baseAmount: "100",
-    conversion: "11910.35",
-    fromCurrency: "Euro",
-    toCurrency: "Japanese Yen"
-  });
+  .respond(
+    {
+      baseAmount: "100",
+      conversion: "11910.35",
+      fromCurrency: "Euro",
+      toCurrency: "Japanese Yen"
+    },
+    200,
+    { "Access-Control-Allow-Origin": "http://localhost:3000" }
+  );
 
 test.requestHooks(conversionMock)(
   "User can convert between two currencies",
