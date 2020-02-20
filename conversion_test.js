95939159b6837e604422ec39e629ecfef9e27250
Dennis Martinez <dennis@dennmart.com>
2020-02-20 14:49:48 +0900

    Setting up conversion mock; Failure due to CORS

--- conversion_test.js
+++ conversion_test.js
@@ -1,18 +1,31 @@
+import { RequestMock } from "testcafe";
 import homePageModel from "./page_models/home_page_model";
 
 fixture("Cash Conversion App").page("http://localhost:3000/");
 
-test("User can convert between two currencies", async t => {
-  await t
-    .typeText(homePageModel.baseAmountInput, "100")
-    .click(homePageModel.fromCurrencySelect)
-    .click(homePageModel.fromCurrencyOptions.withText("Euro"))
-    .click(homePageModel.toCurrencySelect)
-    .click(homePageModel.toCurrencyOptions.withText("Japanese Yen"))
-    .click(homePageModel.convertButton);
+const conversionMock = RequestMock()
+  .onRequestTo("http://localhost:3001/exchange_rates/convert")
+  .respond({
+    baseAmount: "100",
+    conversion: "11910.35",
+    fromCurrency: "Euro",
+    toCurrency: "Japanese Yen"
+  });
 
-  await t.expect(homePageModel.conversionResponse.exists).ok();
-  await t
-    .expect(homePageModel.conversionResponse.innerText)
-    .eql("100 Euro is about 11910.35 Japanese Yen");
-});
+test.requestHooks(conversionMock)(
+  "User can convert between two currencies",
+  async t => {
+    await t
+      .typeText(homePageModel.baseAmountInput, "100")
+      .click(homePageModel.fromCurrencySelect)
+      .click(homePageModel.fromCurrencyOptions.withText("Euro"))
+      .click(homePageModel.toCurrencySelect)
+      .click(homePageModel.toCurrencyOptions.withText("Japanese Yen"))
+      .click(homePageModel.convertButton);
+
+    await t.expect(homePageModel.conversionResponse.exists).ok();
+    await t
+      .expect(homePageModel.conversionResponse.innerText)
+      .eql("100 Euro is about 11910.35 Japanese Yen");
+  }
+);
